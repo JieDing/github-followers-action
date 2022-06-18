766a25a394c7366f88a6ea48f43fa6e857cf38d4
Jie Ding <jie.ding@linkall.com>
2022-06-19 01:03:15 +0800

    feat: support followers sorting
    Signed-off-by: Jie Ding <jie.ding@linkall.com>

--- followers.go
+++ followers.go
@@ -10,6 +10,7 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"sort"
 	"text/template"
 )
 
@@ -19,6 +20,64 @@ var fs embed.FS
 type QL struct {
 	Query string `json:"query"`
 }
+type Follower struct {
+	Login          string
+	Name           string
+	DatabaseID     string
+	FollowingCount float64
+	FollowerCount  float64
+	RepoCredit     float64
+	Contributions  float64
+	TotalCredit    float64
+}
+
+type FollowerArr []Follower
+
+func (f FollowerArr) Len() int {
+	//TODO implement me
+	return len(f)
+}
+
+func isNaN(f float64) bool {
+	return f != f
+}
+func (f FollowerArr) Less(i, j int) bool {
+	//TODO implement me
+	return f[i].TotalCredit < f[j].TotalCredit || (isNaN(f[i].TotalCredit) && !isNaN(f[j].TotalCredit))
+}
+
+func (f FollowerArr) Swap(i, j int) {
+	//TODO implement me
+	f[i], f[j] = f[j], f[i]
+}
+
+func (f Follower) String() string {
+	return "{\n" +
+		"\tLogin: " + f.Login + "\n" +
+		"\tName: " + f.Name + "\n" +
+		"\tDatabaseID: " + f.DatabaseID + "\n" +
+		"\tFollowingCount: " + fmt.Sprintf("%f", f.FollowingCount) + "\n" +
+		"\tFollowerCount: " + fmt.Sprintf("%f", f.FollowerCount) + "\n" +
+		"\tRepoCredit: " + fmt.Sprintf("%f", f.RepoCredit) + "\n" +
+		"\tContributions: " + fmt.Sprintf("%f", f.Contributions) + "\n" +
+		"\tTotalCredit: " + fmt.Sprintf("%f", f.TotalCredit) + "\n" +
+		"}"
+}
+
+func (f *Follower) setTotalCredit() {
+	f.TotalCredit = f.FollowerCount + f.FollowingCount + f.RepoCredit + f.Contributions
+}
+
+func calculateRepoCredit(repos gjson.Result) float64 {
+	var totalCredits float64
+	for _, repo := range repos.Array() {
+		forkCount := repo.Get("forkCount").Float()
+		stargazerCount := repo.Get("stargazerCount").Float()
+		totalCredits += forkCount
+		totalCredits += stargazerCount
+	}
+	return totalCredits
+}
 
 func main() {
 	var login string
@@ -26,6 +85,8 @@ func main() {
 	flag.StringVar(&login, "u", "", "GitHub ID")
 	flag.StringVar(&pat, "p", "", "Personal Access Token")
 	flag.Parse()
+
+	fArr := FollowerArr{}
 
 	client := &http.Client{}
 	url := "https://api.github.com/graphql"
@@ -73,13 +134,35 @@ func main() {
 		}
 		ret := gjson.GetBytes(data, "data.user.followers")
 		pageInfo := ret.Get("pageInfo")
+		followers := ret.Get("nodes")
+		for _, f := range followers.Array() {
+			follower := Follower{
+				Login:          f.Get("login").String(),
+				Name:           f.Get("name").String(),
+				DatabaseID:     f.Get("databaseId").String(),
+				FollowingCount: f.Get("following.totalCount").Float() * 0.1,
+				FollowerCount:  f.Get("followers.totalCount").Float() * 0.35,
+				Contributions:  f.Get("contributionsCollection.contributionCalendar.totalContributions").Float() * 0.2,
+				RepoCredit:     calculateRepoCredit(f.Get("repositories.nodes")) * 0.35,
+			}
+			follower.setTotalCredit()
+			fArr = append(fArr, follower)
+			//fmt.Println(len(fArr))
+		}
+		//fmt.Println(len(fArr))
+
 		hasNextPage = pageInfo.Get("hasNextPage").Bool()
 		after = "after: \"" + pageInfo.Get("endCursor").String() + "\""
 
-		fmt.Println(ret.Get("pageInfo.hasNextPage"))
+		//fmt.Println(ret.Get("pageInfo.hasNextPage"))
 		if !hasNextPage {
 			break
 		}
+	}
+
+	sort.Sort(sort.Reverse(fArr))
+	for _, ff := range fArr {
+		fmt.Println(ff)
 	}
 
 }
